d67693ff0b982a4a90722dec68027dc0926e30ef
Manar-Antar <Manar.antar@Eden-tech.io>
2023-01-10 09:51:07 +0200

    Update

--- src/app/citation/citation.component.ts
+++ src/app/citation/citation.component.ts
@@ -6,34 +6,47 @@ GetCitationStateService
   templateUrl: './citation.component.html',
   styleUrls: ['./citation.component.scss']
 })
-export class CitationComponent {
+export class CitationComponent  {
 
   constructor(private GetCitationStateService: GetCitationStateService) { }
   @ViewChild('inputId') inputId: ElementRef;
+
   dataContainer: any[] = [];
   id: any[] = [];
-  state: string;
+  state: any[] = [];
+  displayState: boolean = false;
 
   getInput() {
-    this.id.push(this.inputId.nativeElement.value)
-    console.log(this.id)
+    this.id.push(this.inputId.nativeElement.value);
+    this.displayState = false;
   }
+
   clearInput() {
     this.inputId.nativeElement.value = '';
   }
 
-  getData(customId) {
+  getData() {
     this.GetCitationStateService.getCitationStateData().subscribe(
       (data) => {
+        this.state=[];
         this.dataContainer = data,
-          console.log(this.dataContainer)
+          console.log(this.dataContainer);
 
         for (let i = 0; i < this.dataContainer.length; i++) {
-          if (this.dataContainer[i].id === customId) this.state=this.dataContainer[i].state
+          for (let j = 0; j < this.id.length; j++) {
+            if (this.dataContainer[i].id === Number(this.id[j])) {
+              this.state.push(this.dataContainer[i].state);
+              console.log(this.dataContainer[i].state);
+            }
+            this.displayState = true;
+          }
         }
+        console.log(this.state)
+        console.log(this.id);
+      
+        
       });
   }
-
 
 
 }
